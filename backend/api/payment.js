67e25e25bd2454b9fda61059f6a86b553730962e
SHRESTHA <83959245+shrestha2401@users.noreply.github.com>
2024-06-02 22:46:26 +0530

    Update payment.js

--- backend/api/payment.js
+++ backend/api/payment.js
@@ -36,7 +36,7 @@ router.post("/order", async (req, res) => {
 router.post("/order/validate", async (req, res) => {
   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
-  const sha = crypto.createHmac("sha256", "bn8zI3LEdRUYIcaiyZo03rtY");
+  const sha = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
   sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
   const digest = sha.digest("hex");
 
